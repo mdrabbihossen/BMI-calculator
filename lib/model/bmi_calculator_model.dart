327fae328bc8994eb52b5dfec1c1035de5aa225b
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2022-10-19 15:42:44 +0600

    calculator model added

--- lib/model/bmi_calculator_model.dart
+++ lib/model/bmi_calculator_model.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 class CalculatorModel {
   final int height;
   final int weight;
+  double? _bmi;
 
   CalculatorModel({
     required this.height,
@@ -11,8 +12,33 @@ class CalculatorModel {
 
   // calculate bmi
   String calculateBMI() {
-    double bmi = weight / pow(height / 100, 2);
-    return bmi.toStringAsFixed(1);
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi!.toStringAsFixed(1);
   }
+
 // calculate bmi ends here
+// get result
+  String getResult() {
+    if (_bmi! >= 25) {
+      return 'Overweight';
+    } else if (_bmi! > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+
+  // get result ends here
+  // get interpretation
+  String getInterpretation() {
+    if (_bmi! >= 25) {
+      return 'You have a higher than normal body weight. Try to exercise more.';
+    } else if (_bmi! >= 18.5) {
+      return 'You have a normal body weight. Good job!';
+    } else {
+      return 'You have a lower than normal body weight. You can eat a bit more.';
+    }
+  }
+// get interpretation ends here
+
 }
